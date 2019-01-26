a8dfc33569d72d2d58e202837795badc0605b147
zuiqiangqishao <727040917@qq.com>
2019-01-26 21:15:51 +0800

    完善交易创建

--- transaction.go
+++ transaction.go
@@ -5,6 +5,7 @@ import (
 	"crypto/sha256"
 	"encoding/gob"
 	"fmt"
+	"os"
 )
 
 const reward = 12.5 //奖励
@@ -25,9 +26,7 @@ func (output *TXOutput) CanBeUnlockWith(unlockData string) bool {
 	return output.ScriptPubKey == unlockData;
 }
 
-func NewTransaction(from, to string, amount float64, bc *BlockChain) {
 
-}
 
 type TXInput struct {
 	TXID      []byte //所引用输出的交易ID
@@ -76,4 +75,48 @@ func (tx *Transaction) SetTXID() {
 	CheckErr(err, "编码transaction失败")
 	hash := sha256.Sum256(buffer.Bytes())
 	tx.TXID = hash[:]
+}
+
+//创建普通交易， send的辅助函数
+func NewTransaction(from, to string, amount float64, bc *BlockChain) *Transaction {
+
+	//map[string][]int64  key : 交易ID   value:引用output的索引数组
+	validUTXOs := make(map[string][]int64)
+	var total float64
+	validUTXOs /*所需要的，合理的utxo的集合*/, total /*返回utxo的金额总和*/ = bc.FindSuitableUTXOs(from, amount)
+
+	//validUTXOs[0x1111111111] = []int64{1}
+	//validUTXOs[0x2222222222] = []int64{0}
+	//...
+	//validUTXOs[0x3333333333] = []int64{0, 4, 8}
+	if total < amount {
+		fmt.Println("余额不足")
+		os.Exit(1)
+	}
+	var inputs []TXInput
+	var outputs []TXOutput
+
+	//1、创建inputs
+	//余额足够，进行output到input的转换
+	//遍历有效的utxo合集
+	for txId, outputIndexes := range validUTXOs {
+		//遍历所有引用的utxo的索引，每一个索引需要创建一个input
+		for _, index := range outputIndexes {
+			input := TXInput{TXID:[]byte(txId), Vout:int64(index), ScriptSig:from}
+			inputs  = append(inputs, input)
+		}
+	}
+
+	//2、创建output
+	output := TXOutput{amount, to}
+	outputs = append(outputs, output)
+
+	if total > amount { //总数大于所需，那么找零
+		output := TXOutput{total-amount, from}
+		outputs = append(outputs, output)
+	}
+
+	tx := Transaction{TXID: []byte{}, TXInputs: inputs, TXOutputs: outputs}
+	tx.SetTXID()
+	return &tx;
 }
